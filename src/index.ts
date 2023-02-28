3e1ab5a35c8e20dd9ea2bcfc3ba11b3c2bb3de57
neicore <neicoreadams@gmail.com>
2023-03-01 02:02:02 +0300

    setup mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
-import * as http from 'node:http'
+import express from 'express'
+import session from 'express-session'
+import * as dotenv from 'dotenv'
+import { MongoClient } from 'mongodb'
+
+dotenv.config()
 
 const PORT = 3009
+const URI = process.env.MONGO_URI
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200)
-    res.end('Habari Dunia!')
+const app = express()
+const client = new MongoClient(URI!)
+
+const connectDb = async () => {
+  try {
+    await client.connect()
+    console.log('Connected successfully to server')
+
+    const database = client.db('test')
+    const collection = database.collection('testers')
+
+    const insertResult = await collection.insertMany([
+      { name: 'tester one' },
+      { a: 'tester two' },
+      { a: 'tester three' },
+    ])
+    console.log('Inserted documents =>', insertResult)
+  } catch (err) {
+    console.error(err, 'mongo err')
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close()
+  }
+}
+
+connectDb()
+
+app.use(
+  session({
+    secret: 'some key',
+    resave: false,
+    saveUninitialized: false,
   })
-  .listen(PORT)
+)
 
-console.log(`Server running at http://127.0.0.1:${PORT}/`)
+app.get('/', (req, res) => {
+  res.send('Habari Dunia')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`)
+})
